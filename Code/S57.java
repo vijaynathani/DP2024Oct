df93bc7ebbb4fc5c9b9e3f9f6e02b1d7bc87e1cc
Vijay Nathani <vijay_nathani@yahoo.com>
2024-10-07 15:47:02 +0530

    Code.

---
+++ Code/S57.java
@@ -0,0 +1,36 @@
+/* This application is about students. Originally we had a
+ * simple Student class:
+ * class Student { String studentId; String name; Date dateOfBirth; }
+ *
+ * Later, in order to record what courses the student has
+ * enrolled in, on which dates he enrolled and how he paid for them,
+ * we modified the code as shown below.
+ * Your task is to implement this requirement without modifying
+ * the Student class.
+ */
+
+class StudentManagementSystem {
+    Student students[];
+    Enrollments en;
+}
+class Student {
+    String studentId;
+    String name;
+    Date dateOfBirth;
+}
+class Enrollment {
+    String courseCode;
+    Date enrollDate;
+    Payment p;
+}
+class Enrollments {
+    Map<Student, Enrollment[]> enrolls;
+    void enroll(Enrollment en) {
+        //add courseCode to courseCodes
+        //add enrollDate to enrollDates
+        //add payment to Payments
+    }
+    void unenroll(Student s, String courseCode) {
+        ...
+    }
+}
